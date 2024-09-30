5fcd03e101e82114eb35d80e3359b2d2bdf6363c
Joseph Paradiso <97202895+JParadiso01@users.noreply.github.com>
2024-09-30 14:06:47 -0400

    cleaned up img2ascii.c

--- img2ascii.c
+++ img2ascii.c
@@ -25,18 +25,17 @@ int main(int argc, char **argv){
 
     char *filename = argv[1];
     int ok, width, height, comps;
-    ok = stbi_info(filename, &width, &height, &comps);
 
+    ok = stbi_info(filename, &width, &height, &comps);
     if (ok == 0){
         printf("ERROR: Image is not a supported format. Please supply one that is.\n");
         exit(1);
     }
-
-    grayscale_data = (unsigned char *)malloc((sizeof(unsigned char))*width*height);  
+ 
     unsigned char *data = stbi_load(filename, &width, &height, &comps, 0);
+    grayscale_data = (unsigned char *)malloc((sizeof(unsigned char))*width*height); 
     grayScale(data, width, height, comps);
-    printf("%u ", grayscale_data[0]);
-
+    
     for (int i = 0; i < width*height; i++){
         int index = (int)(((float)grayscale_data[i]/255)*CHARS_LEN)%CHARS_LEN;
         printf("%c ", chars[index]);
